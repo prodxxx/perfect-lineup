a8cf13eaca05ef3a13e21d8ff719ab83cd95b877
John Carmichael <jc@stackeducation.com>
2020-01-29 17:21:40 -0500

    The solution to this exercise

---
+++ index.js
@@ -0,0 +1,58 @@
+function calculateTotalSalary(lineup) {
+  return lineup.reduce((salary, player) => {
+    return salary + player.salary
+  }, 0)
+}
+
+function getPositionCounts(lineup) {
+  return lineup.reduce((counts, player) => {
+    counts[player.position] = counts[player.position] === undefined ? 1 : counts[player.position] + 1
+
+    return counts
+  }, {})
+}
+
+function getGameCounts(lineup) {
+  return lineup.reduce((games, player) => {
+    games[player.gameId] = games[player.gameId] === undefined ? 1 : games[player.gameId] + 1
+
+    return games
+  }, {})
+}
+
+function getTeamCounts(lineup) {
+  return lineup.reduce((teams, player) => {
+    teams[player.teamId] = teams[player.teamId] === undefined ? 1 : teams[player.teamId] + 1
+
+    return teams
+  }, {})
+}
+
+function violatesGameCount(games) {
+  return Object.values(games).some((count) => { return count > 3 })
+}
+
+function violatesPositionCount(positions) {
+  return positions['P'] !== 1 || positions['C'] !== 1 || positions['1B'] !== 1 ||
+    positions['2B'] !== 1 || positions['3B'] !== 1 || positions['SS'] !== 1 ||
+    positions['OF'] !== 3
+}
+
+function violatesSalary(lineup) {
+  return calculateTotalSalary(lineup) > 45000
+}
+
+function violatesTeamCount(teams) {
+  return Object.values(teams).some((count) => { return count > 2 })
+}
+
+function validateLineup(lineup) {
+  const gameCounts = getGameCounts(lineup)
+  const teamCounts = getTeamCounts(lineup)
+  const positionCounts = getPositionCounts(lineup)
+
+  return !violatesGameCount(gameCounts) && !violatesSalary(lineup) &&
+    !violatesTeamCount(teamCounts) && !violatesPositionCount(positionCounts)
+}
+
+module.exports = validateLineup
